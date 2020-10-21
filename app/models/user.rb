0b1d2e040a8975dcd8a92909029dda5ab24f8975
Raveen K Sharma <raveen08@gmail.com>
2020-10-21 23:58:21 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ActiveRecord::Base
   validates :username, :email, presence: true, uniqueness: true
   
   
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
+end
   
 end
